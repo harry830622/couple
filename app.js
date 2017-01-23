8576517afd76062e47131e2dea53b9db5fac260b
Harry Chang <harry830622@gmail.com>
2017-01-23 15:12:47 +0800

    Quick reply to ask for priority

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const http = require('http');
 
 const ig = require('./ig.js');
+const Map = require('./map.js');
 const Bot = require('./bot.js');
 
 const app = express();
+
+const map = new Map({ key: process.env.google_api_key });
 
 const bot = new Bot(app, {
   appSecret: process.env.app_secret,
@@ -25,11 +28,55 @@ bot.on('message', (payload, reply) => {
   if (text.includes('instagram.com/p/')) {
     const url = text;
 
+    const result = {};
     bot.sendSenderAction(recipient, 'typing_on')
       .then(() => ig.loadLocation(url))
       .then((location) => {
-        reply({ text: location });
+        Object.assign(result, { location });
       })
+      .then(() => map.address(result.location))
+      .then((body) => {
+        const address = body.results[0].formatted_address;
+        Object.assign(result, { address });
+      })
+      .then(() => ig.loadImage(url))
+      .then((image) => {
+        Object.assign(result, { image });
+      })
+      .then(() => reply({
+        attachment: {
+          type: 'template',
+          payload: {
+            template_type: 'generic',
+            elements: [
+              {
+                image_url: result.image,
+                title: result.location,
+                subtitle: result.address,
+                default_action: {
+                  url,
+                  type: 'web_url',
+                },
+              },
+            ],
+          },
+        },
+      }))
+      .then(() => reply({
+        text: '想吃？',
+        quick_replies: [
+          {
+            content_type: 'text',
+            title: '超想吃！',
+            payload: JSON.stringify({ priority: 5 }),
+          },
+          {
+            content_type: 'text',
+            title: '先記著再說哈哈',
+            payload: JSON.stringify({ priority: 3 }),
+          },
+        ],
+      }))
       .then(() => bot.sendSenderAction(recipient, 'typing_off'))
       .catch((err) => {
         reply({ text: err.message });
---
+++ map.js
@@ -0,0 +1,22 @@
+const rp = require('request-promise-native');
+
+class Map {
+  constructor({ key }) {
+    this.key = key;
+  }
+
+  address(query) {
+    return rp({
+      uri: 'https://maps.googleapis.com/maps/api/place/textsearch/json',
+      qs: {
+        query,
+        key: this.key,
+        language: 'zh-TW',
+      },
+      json: true,
+    });
+  }
+
+}
+
+module.exports = Map;
